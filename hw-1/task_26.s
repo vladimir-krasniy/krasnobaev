e91ebc0779c1fe6edb5e690052833c2445f48934
vladimir-krasniy <vladimir-krasniy@mail.ru>
2014-03-29 00:23:37 +0300

    Does not work for unrevealed reason

---
+++ hw-1/task_26.s
@@ -0,0 +1,56 @@
+  .data
+        .globl main
+
+add:
+        .space 4
+arr:
+        .int 3, 34, 23, 22, 3, 9, 23, 77, 35
+min:
+        .int
+input_string:
+        .string "%d"
+out_string:
+        .string "%d\n"
+
+        .text
+main:
+//Epilogue
+        pushl   %ebp
+        movl    %esp, %ebp
+
+//Input
+        pushl $add
+        pushl $input_string
+        call scanf
+        addl $8, %esp
+
+
+        movl    $0, %edi
+        movl    arr(, %edi, 4), %eax
+
+
+compr:
+        cmpl    $9, %edi
+        jae     finishing
+
+        cmpl    arr(, %edi, 4), %eax
+        jb      minim
+
+        movl    arr(, %edi, 4), %eax
+
+
+minim:
+        incl    %edi
+        jmp     compr
+
+finishing:
+        movl    %eax, min
+        pushl   $32
+        pushl   $input_string
+        call    printf
+        addl    $8, %esp
+
+        movl    $0, %eax
+        popl    %ebp
+
+        ret
